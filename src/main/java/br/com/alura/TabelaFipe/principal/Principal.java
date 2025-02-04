a575eaefd83d9ff1b9224f2334d8aff0183f7c33
eduardo-toste <duciochetti@gmail.com>
2025-02-04 19:58:49 -0300

    implementing the code

--- src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -1,7 +1,11 @@
 package br.com.alura.TabelaFipe.principal;
 
+import br.com.alura.TabelaFipe.model.Dados;
+import br.com.alura.TabelaFipe.model.Modelos;
 import br.com.alura.TabelaFipe.service.ConsumoApi;
+import br.com.alura.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
@@ -9,6 +13,7 @@ public class Principal {
     private ConsumoApi consumoApi = new ConsumoApi();
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
     private String endereco;
+    private ConverteDados conversor = new ConverteDados();
 
     public void exibeMenu() {
         String menu = """
@@ -35,5 +40,30 @@ public class Principal {
 
         var json = consumoApi.obterDados(endereco);
         System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o codigo da marca para consulta: ");
+        var codigoMarca = scanner.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+        json = consumoApi.obterDados(endereco);
+
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("\nInsira o nome do veiculo que deseja buscar: ");
+        var nomeVeiculo = scanner.nextLine();
+
+        modeloLista.modelos().stream()
+                .filter(m -> m.nome().contains(nomeVeiculo))
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
     }
 }
